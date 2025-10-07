219ebc494cd2a945687a267208e6bf3874f2032f
Chirayu Sukhum <tueychirayu@gmail.com>
2025-10-07 15:54:48 +0700

    Add backend code as part of monorepo

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -5,3 +5,4 @@ from flask_cors import CORS
 db = SQLAlchemy()
 migrate = Migrate()
 cors = CORS()
+
--- backend/app/models.py
+++
@@ -1,5 +0,0 @@
-from .extensions import db
-from decimal import Decimal
-
-# Example money column (works on SQLite & other DBs)
-Money = db.Numeric(14, 2, asdecimal=True)
